0ac044603e9c3770edde8f9a6df58324d8a8288b
Sergio27 <sergiohumberto27@gmail.com>
2016-10-09 10:40:38 -0600

    GIF v3
    Tercera version del proyecto de GIF, donde se añade la lectura y el
    guardado de todos los campos a la lista correpondiente que lleva el
    control de los GIF

---
+++ GIF v3/src/Nodo.java
@@ -0,0 +1,166 @@
+
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SERGIO
+ */
+public class Nodo {
+    String nombre;
+    String version;
+    int ancho;
+    int altura;
+    int cantidadcolores;
+    String compresion;
+    private int[] formatonumerico;
+    Color colorfondo;
+    int cantidadimagenes;
+    String fechacreacion;
+    String fechamodificacion;
+    String comentarios;
+    Nodo siguiente;
+    Nodo anterior;
+
+    public Nodo() {
+        this.nombre = "";
+        this.version = "";
+        this.altura = 0;
+        this.ancho = 0;
+        this.cantidadcolores = 0;
+        this.compresion = "";
+        this.colorfondo = new Color(0, 0, 0);
+        this.cantidadimagenes = 0;
+        this.fechacreacion = "";
+        this.fechamodificacion = "";
+        this.comentarios = "";
+        this.siguiente = null;
+        this.anterior = null;
+        this.formatonumerico=new int[0];
+    }
+    /**
+     * Inicializa el arreglo String iniciarformatonumerico
+     * @param n Tamaño del arreglo
+     */
+    public void inciarformatonumerico(int n){
+        String[] formatonumerico = new String[n];
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getVersion() {
+        return version;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getCantidadcolores() {
+        return cantidadcolores;
+    }
+
+    public String getCompresion() {
+        return compresion;
+    }
+
+    public int[] getFormatonumerico() {
+        return formatonumerico;
+    }
+
+    public Color getColorfondo() {
+        return colorfondo;
+    }
+
+    public int getCantidadimagenes() {
+        return cantidadimagenes;
+    }
+
+    public String getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public String getFechamodificacion() {
+        return fechamodificacion;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setCantidadcolores(int cantidadcolores) {
+        this.cantidadcolores = cantidadcolores;
+    }
+
+    public void setCompresion(String compresion) {
+        this.compresion = compresion;
+    }
+
+    public void setFormatonumerico(int[] formatonumerico) {
+        this.formatonumerico = formatonumerico;
+    }
+
+    public void setColorfondo(Color colorfondo) {
+        this.colorfondo = colorfondo;
+    }
+
+    public void setCantidadimagenes(int cantidadimagenes) {
+        this.cantidadimagenes = cantidadimagenes;
+    }
+
+    public void setFechacreacion(String fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    public void setFechamodificacion(String fechamodificacion) {
+        this.fechamodificacion = fechamodificacion;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+    
+}
